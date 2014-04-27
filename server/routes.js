920e048537fc3660459f1c9fc210a552de6ccae6
Tyler Henkel <tylerhenkel@hotmail.com>
2014-04-26 21:44:39 -0400

    Condense catch-all 404 routing

--- server/routes.js
+++ server/routes.js
@@ -14,18 +14,8 @@ module.exports = function(app) {
   app.use('/api/users', require('./components/user'));
 
   // All undefined asset or api routes should return a 404
-  app.route('/api/*')
-    .get(errors[404]);
-  app.route('/components/*')
-    .get(errors[404]);
-  app.route('/scripts/*')
-    .get(errors[404]);
-  app.route('/styles/*')
-    .get(errors[404]);
-  app.route('/bower_components/*')
-    .get(errors[404]);
-  app.route('/images/*')
-    .get(errors[404]);
+  app.route('/:url(api|components|scripts|styles|bower_components|images)/*')
+   .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
