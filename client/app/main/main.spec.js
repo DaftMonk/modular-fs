85f30b5c30d22ed7e5d665e13cddffd1ae4b8064
Tyler Henkel <tylerhenkel@hotmail.com>
2014-05-09 23:28:28 -0400

    Add mock for socket service so tests work

--- client/app/main/main.spec.js
+++ client/app/main/main.spec.js
@@ -4,6 +4,7 @@ describe('Controller: MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('ngApp'));
+  beforeEach(module('socketMock'));
 
   var MainCtrl,
       scope,
