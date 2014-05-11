613d4f4e6232fdf1e8c2a6f6c46f55f70b3293e1
Tyler Henkel <tylerhenkel@hotmail.com>
2014-05-11 19:36:35 -0400

    Update style

--- server/socketio.js
+++ server/socketio.js
@@ -4,21 +4,6 @@
 
 'use strict';
 
-// When the user disconnects.. perform this
-function onDisconnect(socket) {
-}
-
-// When the user connects.. perform this
-function onConnect(socket) {
-   // When the client emits 'info', this listens and executes
-  socket.on('info', function (data) {
-    console.info('[%s] %s', socket.address, JSON.stringify(data, null, 2));
-  });
-
-  // Register listeners for components
-  require('./api/thing/thing.socket').register(socket);
-}
-
 // Setup socket.io
 module.exports = function (socketio) {
   // The amount of detail that the server should output to the logger.
@@ -44,3 +29,18 @@ module.exports = function (socketio) {
     console.info('[%s] CONNECTED', socket.address);
   });
 };
+
+// When the user disconnects.. perform this
+function onDisconnect(socket) {
+}
+
+// When the user connects.. perform this
+function onConnect(socket) {
+   // When the client emits 'info', this listens and executes
+  socket.on('info', function (data) {
+    console.info('[%s] %s', socket.address, JSON.stringify(data, null, 2));
+  });
+
+  // Register listeners for components
+  require('./api/thing/thing.socket').register(socket);
+}
