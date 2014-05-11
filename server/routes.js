502a2d2300b47ff709e810d0f45cc89e1d3af0a3
Tyler Henkel <tylerhenkel@hotmail.com>
2014-05-11 19:24:04 -0400

    Cleanup express configuration

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
-  app.route('/')
+  app.route('/*')
     .get(function(req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
